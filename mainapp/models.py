5054ed2c17490102998d5d90e09e03945ad2c052
Alexey <vaznoe2024@mail.ru>
2025-09-07 17:10:21 +0300

    v.3

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Table(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default = '')
+    contant = models.TextField(default='')
+    number = models.FloatField(default='')
+    date = models.DateField(default='datetime.date.today')
